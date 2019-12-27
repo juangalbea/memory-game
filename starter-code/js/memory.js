5fc90d7b678e9e91911dd143210db47426e80d24
juangalbea <juangalbea@gmail.com>
2019-12-27 23:12:43 +0100

    pass all the tests

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,35 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards, pickedCards, pairsClicked, pairsGuessed){
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  pickedCards
+  shuffleCards(cards) {
+    let len = this.cards.length;
+        while (len > 0) {
+            len--;
+            let temp = this.cards[len];
+            let randomIdx = Math.floor(Math.random() * len);
+            this.cards[len] = this.cards[randomIdx];
+            this.cards[randomIdx] = temp;
+        }
+      }
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isFinished() {
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
